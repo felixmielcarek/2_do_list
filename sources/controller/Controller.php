0b7ce8c45ab47f9e3c0b409c7352864ed3dfd060
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2022-11-25 08:45:27 +0100

    Réécriture du controller

--- sources/controller/Controller.php
+++ sources/controller/Controller.php
@@ -1,89 +1,60 @@
 <?php
 
-class Controleur
+class Controller
 {
 
     function __construct()
     {
         global $rep, $vues;
-// on démarre ou reprend la session si necessaire (préférez utiliser un modèle pour gérer vos session ou cookies)
+        // on démarre ou reprend la session si necessaire (préférez utiliser un modèle pour gérer vos session ou cookies)
         session_start();
+        //debut
 
-
-//debut
-
-//on initialise un tableau d'erreur
+        //on initialise un tableau d'erreur
         $dVueEreur = array();
 
         try {
             $action = $_REQUEST['action'];
-
             switch ($action) {
-
                 case NULL:
-                    $this->Reinit();
+                    $this->GoHome();
                     break;
-
-
-                case "validationFormulaire":
-                    $this->ValidationFormulaire($dVueEreur);
+                case "addPbLists":
+                    $this->AddPbLists();
                     break;
-
-//mauvaise action
                 default:
                     $dVueEreur[] = "Erreur d'appel php";
                     require($rep . $vues['vuephp1']);
                     break;
             }
-
         } catch (PDOException $e) {
             //si erreur BD, pas le cas ici
             $dVueEreur[] = "Erreur inattendue!!! ";
             require($rep . $vues['erreur']);
-
         } catch (Exception $e2) {
             $dVueEreur[] = "Erreur inattendue!!! ";
             require($rep . $vues['erreur']);
         }
-
-
-//fin
+        //fin
         exit(0);
     }//fin constructeur
 
 
-    function Reinit()
+    function GoHome()
     {
         global $rep, $vues; // nécessaire pour utiliser variables globales
-
-        $dVue = array(
+        /*$dVue = array(
             'nom' => "",
             'age' => 0,
-        );
-        require($rep . $vues['vuephp1']);
+        );*/
+        require($rep . $vues['home.php']);
     }
 
-    function ValidationFormulaire(array $dVueEreur)
+    function AddPbLists()
     {
-        global $rep, $vues;
-
-
-//si exception, ca remonte !!!
-        $nom = $_POST['txtNom']; // txtNom = nom du champ texte dans le formulaire
-        $age = $_POST['txtAge'];
-        Validation::val_form($nom, $age, $dVueEreur);
-
-        $model = new Simplemodel();
-        $data = $model->get_data();
-
-        $dVue = array(
-            'nom' => $nom,
-            'age' => $age,
-            'data' => $data,
-        );
-        require($rep . $vues['vuephp1']);
+        global $rep, $vues; // nécessaire pour utiliser variables globales
+        require($rep . $vues['add.php']);
     }
-
 }//fin class
 
 ?>
