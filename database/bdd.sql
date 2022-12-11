65bd540b29939b396e582debe54b316c834f17ef
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2022-12-11 13:28:27 +0100

    Changement script sql

--- database/bdd.sql
+++ database/bdd.sql
@@ -1,25 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : sam. 10 déc. 2022 à 15:10
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
+-- Structure de la table `lists`
+--
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP TABLE IF EXISTS `lists`;
+CREATE TABLE IF NOT EXISTS `lists`
+(
+    `id`             mediumint(9) NOT NULL AUTO_INCREMENT,
+    `idAuthor`       mediumint(9) NOT NULL,
+    `title`          varchar(50)  NOT NULL,
+    `description`    varchar(100) DEFAULT NULL,
+    `dateOfCreation` date         NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 27
+  DEFAULT CHARSET = latin1;
 
 --
--- Base de données : `2dolist`
+-- Déchargement des données de la table `lists`
 --
+
+INSERT INTO `lists` (`id`, `idAuthor`, `title`, `description`, `dateOfCreation`)
+VALUES (20, 1, 'Ranger le salon', 'Je veux que vous fassiez le mÃ©nage avant que je rentre', '2022-12-07'),
+       (25, 1, 'azeaze', 'azeaze', '2022-12-10');
 
 -- --------------------------------------------------------
 
@@ -28,23 +30,28 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE IF NOT EXISTS `tasks` (
-    `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-    `content` varchar(50) NOT NULL,
-    `isDone` tinyint(1) NOT NULL DEFAULT '0',
-    `idList` mediumint(9) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tasks`
+(
+    `id`      mediumint(9) NOT NULL AUTO_INCREMENT,
+    `content` varchar(50)  NOT NULL,
+    `isDone`  tinyint(1)   NOT NULL DEFAULT '0',
+    `idList`  mediumint(9) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `idList` (`idList`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 47
+  DEFAULT CHARSET = latin1;
 
 --
 -- Déchargement des données de la table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `content`, `isDone`, `idList`) VALUES
-                                                              (38, 'Demander Ã  Dorian', 0, 20),
-                                                              (39, 'Demander Ã  Lucas', 0, 20),
-                                                              (41, 'azeazazeaze', 0, 20);
+INSERT INTO `tasks` (`id`, `content`, `isDone`, `idList`)
+VALUES (38, 'Demander Ã  Dorian', 1, 20),
+       (39, 'Demander Ã  Lucas', 1, 20),
+       (41, 'azeazazeaze', 1, 20),
+       (43, 'azeaze', 0, 20),
+       (44, 'azeaze', 1, 25);
 
 --
 -- Contraintes pour les tables déchargées
@@ -57,6 +64,6 @@ ALTER TABLE `tasks`
     ADD CONSTRAINT `idList` FOREIGN KEY (`idList`) REFERENCES `lists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
